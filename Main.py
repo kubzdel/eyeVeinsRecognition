ad5b2e83cfe48eadb9e77a813ad4f33485f503fe
kubzdel <kubzdel@gmail.com>
2018-05-08 02:21:40 +0200

    Reformatted.

--- Main.py
+++ Main.py
@@ -19,95 +19,80 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-def chunkify(lst,n):
+
+def chunkify(lst, n):
     return [lst[i::n] for i in xrange(n)]
 
-images =[]
-masks =[]
-k=10
-X=[]
-Y=[]
-Xtest=[]
-Ytest=[]
-MAX_HEIGHT=683
-MAX_WIDTH=1024
-MASK_SIZE=25 #musi być nieparzysta
-PHOTO_SAMPLES=25000
-#ładuj zdjęcia
-directory = os.getcwd()+"\Small" + '/'
+
+images = []
+masks = []
+k = 10
+X = []
+Y = []
+Xtest = []
+Ytest = []
+MASK_SIZE = 25  # musi być nieparzysta
+PHOTO_SAMPLES = 25000
+# ładuj zdjęcia
+directory = os.getcwd() + "\Small" + '/'
 for file in os.listdir("Small"):
     if file.endswith(".jpg"):
-        jpgfile = img.load(directory+file,True)
+        jpgfile = img.load(directory + file, True)
         images.append(array(jpgfile))
     if file.endswith(".tif"):
-        tiffile = Image.open("Small/"+file)
+        tiffile = Image.open("Small/" + file)
         masks.append(array(tiffile))
     if file.endswith(".png"):
-        pngfile =  img.load(directory+file,True)
+        pngfile = img.load(directory + file, True)
         masks.append(array(pngfile))
 # plt.imshow(array(masks[1]))
 # plt.show()
 for mask in masks:
     for i in range(len(mask)):
         for j in range(len(mask[i])):
-            if mask[i][j]>10:
-                mask[i][j]=255
+            if mask[i][j] > 10:
+                mask[i][j] = 255
             else:
-                mask[i][j]=0
-
+                mask[i][j] = 0
 
 config = tf.ConfigProto()
-config.gpu_options.allow_growth=True
+config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 
-for image,mask in zip(images,masks):
-    for i in range(0,PHOTO_SAMPLES):
+for image, mask in zip(images, masks):
+    for i in range(0, PHOTO_SAMPLES):
         flag = -1;
         veinFlag = rand.randint(0, 2)
         while (flag != veinFlag):
-            startX = rand.randint(5,image.shape[0]-(MASK_SIZE+5))
-            endX = startX+MASK_SIZE
-            startY = rand.randint(5,image.shape[1]-(MASK_SIZE+5))
-            endY = startY+MASK_SIZE
-            centerX=startX+int(MASK_SIZE/2)
-            centerY=startY+int(MASK_SIZE/2)
-            sample = image[startX:endX,startY:endY]
-            sample = array(sample).reshape(MASK_SIZE,MASK_SIZE,1)
-            temp = mask[centerX,centerY]
-            result=[]
+            startX = rand.randint(5, image.shape[0] - (MASK_SIZE + 5))
+            endX = startX + MASK_SIZE
+            startY = rand.randint(5, image.shape[1] - (MASK_SIZE + 5))
+            endY = startY + MASK_SIZE
+            centerX = startX + int(MASK_SIZE / 2)
+            centerY = startY + int(MASK_SIZE / 2)
+            sample = image[startX:endX, startY:endY]
+            sample = array(sample).reshape(MASK_SIZE, MASK_SIZE, 1)
+            temp = mask[centerX, centerY]
+            result = []
             if (temp == 255):
                 result = [1, 0]
                 flag = rand.randint(1, 2)
             else:
                 result = [0, 1]
                 flag = 0
-       # result = array(result).reshape(2)
+        # result = array(result).reshape(2)
         X.append(sample)
         Y.append(result)
 
-X,Y  = shuffle(X,Y)
+X, Y = shuffle(X, Y)
 
-
-splittedSamples = chunkify(X,k)
-splittedMasks = chunkify(Y,k)
+splittedSamples = chunkify(X, k)
+splittedMasks = chunkify(Y, k)
 
 timeStamp = datetime.datetime.now().time()
-# Make sure the data is normalized
 
-
-# Create extra synthetic training data by flipping, rotating and blurring the
-# images on our data set.
-img_aug = ImageAugmentation()
-img_aug.add_random_flip_leftright()
-img_aug.add_random_rotation(max_angle=45.)
-img_aug.add_random_blur(sigma_max=2.)
-
-
-# Wrap the network in a model object
-
-
-averageAcc= 0;
-for i in range(0,k):
+averageAcc = 0;
+for i in range(0, k):
 
     trainSamples = []
     testSamples = []
@@ -119,7 +104,6 @@ for i in range(0,k):
 
     testSamples.extend(splittedSamplesTemp.pop(i))
     testMasks.extend(splittedMasksTemp.pop(i))
-
 
     trainSamples = [item for sublist in splittedSamplesTemp for item in sublist]
     trainMasks = [item for sublist in splittedMasksTemp for item in sublist]
@@ -134,73 +118,52 @@ for i in range(0,k):
     img_prep.add_featurewise_zero_center()
     img_prep.add_featurewise_stdnorm()
     network = input_data(shape=[MASK_SIZE, MASK_SIZE, 1],
-                         data_preprocessing=img_prep,name=('InputData'+str(i)))
+                         data_preprocessing=img_prep, name=('InputData' + str(i)))
     print(network)
 
-    # Step 1: Convolution
     network = conv_2d(network, 32, 3, activation='relu')
 
-    # Step 2: Max pooling
     network = max_pool_2d(network, 2)
 
-    # Step 3: Convolution again
     network = conv_2d(network, 64, 3, activation='relu')
 
-    # Step 4: Convolution yet again
     network = conv_2d(network, 64, 3, activation='relu')
 
-    # Step 5: Max pooling again
     network = max_pool_2d(network, 2)
 
-    # Step 6: Fully-connected 1024 node neural network
     network = fully_connected(network, 1024, activation='relu')
 
-    # Step 7: Dropout - throw away some data randomly during training to prevent over-fitting
     network = dropout(network, 0.5)
 
-    # Step 8: Fully-connected neural network with two outputs (0=isn't a bird, 1=is a bird) to make the final prediction
     network = fully_connected(network, 2, activation='softmax')
 
-    # Tell tflearn how we want to train the network
     network = regression(network, optimizer='adam',
                          loss='categorical_crossentropy',
                          learning_rate=0.001)
 
-
     model = tflearn.DNN(network, tensorboard_verbose=0)
-    model.fit(X, Y, n_epoch=4, shuffle=True, validation_set=(Xtest, Ytest),
-                  show_metric=True,batch_size=64,snapshot_epoch=True)
-
-
-    # Save model when training is complete to a file
+    model.fit(X, Y, n_epoch=3, shuffle=True, validation_set=(Xtest, Ytest),
+              show_metric=True, batch_size=64, snapshot_epoch=True)
 
     accuracy = 0
 
-
-
-    for  toPredict, actual in zip(Xtest,Ytest):
+    for toPredict, actual in zip(Xtest, Ytest):
         prediction = model.predict([toPredict])
         predicted_class = np.argmax(prediction)
         actual_class = np.argmax(actual)
-        if(predicted_class == actual_class):
-            accuracy+=1
+        if (predicted_class == actual_class):
+            accuracy += 1
     accuracy = accuracy / len(Ytest)
-    averageAcc+=accuracy
-    print("Acucracy: "+str(accuracy))
+    averageAcc += accuracy
+    print("Acucracy: " + str(accuracy))
 
-
-    model.save("docelowy/"+str(accuracy)+" eye-veins-classifier.tfl")
+    model.save("docelowy2/" + str(accuracy) + " eye-veins-classifier.tfl")
 
     print("Network trained and saved as eye-veinsclassifier.tfl!")
     tf.reset_default_graph()
 
-averageAcc =averageAcc/k
+averageAcc = averageAcc / k
 print(averageAcc)
-text_file = open("docelowy/averageACC.txt", "w")
-text_file.write("Średnia dokładność: "+str(averageAcc))
+text_file = open("docelowy2/averageACC.txt", "w")
+text_file.write("Średnia dokładność: " + str(averageAcc))
 text_file.close()
-
-
-
-
-
--- reconstruct.py
+++ reconstruct.py
@@ -1,4 +1,4 @@
-  # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 from __future__ import division, print_function, absolute_import
 
 import matplotlib.pyplot as plt
@@ -12,47 +12,32 @@ import numpy as np
 from numpy import array
 import os
 from skimage import data as img
+
 img_prep = ImagePreprocessing()
 img_prep.add_featurewise_zero_center()
 img_prep.add_featurewise_stdnorm()
 
-# Create extra synthetic training data by flipping, rotating and blurring the
-# images on our data set.
-img_aug = ImageAugmentation()
-img_aug.add_random_flip_leftright()
-img_aug.add_random_rotation(max_angle=25.)
-img_aug.add_random_blur(sigma_max=3.)
-
-MASK_SIZE=25
+MASK_SIZE = 25
 network = input_data(shape=[MASK_SIZE, MASK_SIZE, 1],
                      data_preprocessing=img_prep)
 print(network)
 
-# Step 1: Convolution
 network = conv_2d(network, 32, 3, activation='relu')
 
-# Step 2: Max pooling
 network = max_pool_2d(network, 2)
 
-# Step 3: Convolution again
 network = conv_2d(network, 64, 3, activation='relu')
 
-# Step 4: Convolution yet again
 network = conv_2d(network, 64, 3, activation='relu')
 
-# Step 5: Max pooling again
 network = max_pool_2d(network, 2)
 
-# Step 6: Fully-connected 512 node neural network
 network = fully_connected(network, 1024, activation='relu')
 
-# Step 7: Dropout - throw away some data randomly during training to prevent over-fitting
 network = dropout(network, 0.5)
 
-# Step 8: Fully-connected neural network with two outputs (0=isn't a bird, 1=is a bird) to make the final prediction
 network = fully_connected(network, 2, activation='softmax')
 
-# Tell tflearn how we want to train the network
 network = regression(network, optimizer='adam',
                      loss='categorical_crossentropy',
                      learning_rate=0.001)
@@ -60,33 +45,32 @@ network = regression(network, optimizer='adam',
 # Wrap the network in a model object
 model = tflearn.DNN(network, tensorboard_verbose=0, checkpoint_path='eye-veins.tfl.ckpt')
 model.load("eye-veins-classifier.tfl")
-directory = os.getcwd()+'/'
+directory = os.getcwd() + '/'
 # Load the image file
-img = array(img.load(directory+"test2.jpg",True))
-reconstructed=np.zeros((img.shape[0],img.shape[1]))
+img = array(img.load(directory + "test2.jpg", True))
+reconstructed = np.zeros((img.shape[0], img.shape[1]))
 
-for x in range(0,img.shape[0]-MASK_SIZE-1):
+for x in range(0, img.shape[0] - MASK_SIZE - 1):
     print(x)
-    for y in range(0,img.shape[1]-MASK_SIZE-1):
+    for y in range(0, img.shape[1] - MASK_SIZE - 1):
         centerX = x + int(MASK_SIZE / 2)
         centerY = y + int(MASK_SIZE / 2)
-        sample = img[x:(x+MASK_SIZE), y:(y+MASK_SIZE)]
+        sample = img[x:(x + MASK_SIZE), y:(y + MASK_SIZE)]
         sample = array(sample).reshape(MASK_SIZE, MASK_SIZE, 1)
         prediction = model.predict([sample])
         result = prediction.T[0]
 
-        if result>0.4:
-             reconstructed[centerX][centerY]=1
+        if result > 0.4:
+            reconstructed[centerX][centerY] = 1
         else:
-             reconstructed[centerX][centerY]=0
-
+            reconstructed[centerX][centerY] = 0
 
 # Predict
 reconstructed = array(reconstructed)
-np.save("plik2",reconstructed)
-#reconstructed = np.load("plik.npy")
-#PostProcessing
-#reconstructed = morph.dilation(morph.erosion(reconstructed))
+np.save("plik2", reconstructed)
+# reconstructed = np.load("plik.npy")
+# PostProcessing
+# reconstructed = morph.dilation(morph.erosion(reconstructed))
 # reconstructed = np.load("plik2.npy")
 plt.imshow(reconstructed)
 plt.show()
